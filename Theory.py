49462b549889cc63e01d8b44b80e31013d1a9d73
JRic <ariazjean@gmail.com>
2022-02-15 17:33:34 -0400

    changue1

---
+++ Theory.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+# a = [5,2,5,'holaMundo','otroString']
+
+            # id()
+            # list()
+            # min()
+            # max()
+            # len()
+        # del array
+            # append()
+            # count()
+            # extend()
+            # index()
+            # sort()
+            # remove()
+            # pop()
+            # reverse()
+            # sort()
+
+
+# print(a[0]+a[0])
+
+# print(a.count(5))
+
+
+objetos = {'saludo':'jsjsjsjs'}
+objetos['saludo'] = "hola"
+
+objetos.append('hola')
+print(objetos['saludo'])
